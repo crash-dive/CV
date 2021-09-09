7e82a2ee4e53a3f84795e587f72884d2553aee6d
crash-dive <crash-dive@users.noreply.github.com>
2021-09-09 14:10:35 +0000

    deploy: c5fc777015505bdbaf4b57ef48570bcbf1f2a940

--- js/nav-menu.min.b59ebe04379f3b4ce37350dfa016e4fb5f63a81b860d02031c9e9642ac6dd8cfc612e6725238c5048aad45650def44e4808cb8cf4023578c8bedc90035963fa4.js
+++
@@ -1,1 +0,0 @@
-let navButton=document.getElementById('nav-button'),navMenu=document.getElementById('nav-menu'),navMenuFrame=document.getElementById('nav-menu-frame');navButton.addEventListener('click',function(){navButton.classList.toggle('is-open'),navButton.classList.toggle('is-closed'),navMenu.classList.toggle('is-open'),navMenuFrame.classList.toggle('is-open')})
